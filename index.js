c60bb6cb2f0e4a1b1625c27134397cd40df5b61f
Muhamedyehya <109024550+Muhamedyehya@users.noreply.github.com>
2025-12-23 17:00:12 +0200

    Initial backend commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import adRoute from "./routes/adRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB()
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+
+app.get("/", (req, res) => res.send("API is running"));
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/ads", adRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
